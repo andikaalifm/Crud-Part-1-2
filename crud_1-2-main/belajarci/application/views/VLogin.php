e3cf7b85c36a6b6be0fba47d0a8ef9090b401ac4
unknown <sylfanifebrianti@gmail.com>
2021-11-21 20:30:08 -0800

    Crud part 1-2

---
+++ crud_1-2-main/belajarci/application/views/VLogin.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>Login System Maul</title>
+	<link rel="stylesheet" href="/application/views/style.css?v=1.1">
+</head>
+<body>
+	<div class="content-login">
+	<table align="center" cellspacing="0" width="70%" style="margin: auto; padding: 16px">
+		<form action="" method="post">
+		<tr>
+			<td colspan="2"><h1 align="center">Login</h1></td>
+		</tr>
+		<tr>
+			<td>Username</td>
+			<td><input type="text" name="txt_user"></td>
+		</tr>
+		<tr>
+			<td>Passsword</td>
+			<td><input type="passsword" name="txt_pass"></td>
+		</tr>
+		<tr>
+			<td colspan="2" style="padding-bottom: 16px">
+				<input type="reset" name="btn_reset" value="Reset">
+				<input type="submit" name="btn_login" value="Login">
+			</td>
+		</tr>
+	</form>
+	</table>
+</div>
+</body>
+</html>
---
+++ crud_1-2-main/belajarci/dbs_cobaci.sql
@@ -0,0 +1,87 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 16, 2021 at 10:03 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbs_cobaci`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_login`
+--
+
+CREATE TABLE `tbl_login` (
+  `nis` int(11) NOT NULL,
+  `username` varchar(35) NOT NULL,
+  `password` varchar(35) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_login`
+--
+
+INSERT INTO `tbl_login` (`nis`, `username`, `password`) VALUES
+(1122, 'aa', 'bb'),
+(1325, 'maul', 'maul123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_siswa`
+--
+
+CREATE TABLE `tbl_siswa` (
+  `nis` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_siswa`
+--
+
+INSERT INTO `tbl_siswa` (`nis`, `nama`, `alamat`) VALUES
+(1122, 'Muhamad Maulana Ramdani', 'Kota Bogor');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  ADD PRIMARY KEY (`nis`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_siswa`
+--
+ALTER TABLE `tbl_siswa`
+  MODIFY `nis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1124;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
